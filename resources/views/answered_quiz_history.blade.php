9850a45612950d65325b66c5cb4e2cbea45ba709
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-08-09 00:23:08 -0700

    history for last 3 days

--- resources/views/answered_quiz_history.blade.php
+++ resources/views/answered_quiz_history.blade.php
@@ -13,49 +13,51 @@
         <div class="recent-sales box">
        <div class="title">Answered Quiz History</div>  
         <div class="sales-details">
-            @if (count($QuizResult ) > 0)
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th> # </th>
-                            <th>Quiz</th>
-                            <th>Score</th>
-                            <th>Time Taken</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        @if (count($QuizResult) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Quiz</th>
+                        <th>Score</th>
+                        <th>Time Taken</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                    $rowNumber = 1;
+                    @endphp
+                    @foreach ($QuizResult as $result)
                         @php
-                        $rowNumber = 1;
+                        $answeredAt = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $result->answered_at);
+                        $hoursAgo = $answeredAt->diffInHours(now());
                         @endphp
-                        @foreach ($QuizResult as $result)
-                        <tr class="topic">
-                            <td class="font-weight-medium"> {{$rowNumber}} </td>
-                            <td>{{ $result->quiz->title }}</td>
-                            <td>{{ $result->score }}/  {{ $result->quiz->questions->count() }}</td>
-                            @if ($result->answered_at)
-                            <?php
-                            $answeredAt = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $result->answered_at);
-                            $hoursAgo = $answeredAt->diffInHours(now());
-                            ?>
-                            <td><span class="timestamp">@if ($hoursAgo === 0) just now
-                                @else
-                                    {{ $hoursAgo }} hours ago
-                                @endif
-                            </span></td>
-                            @else
-                            <td><span class="timestamp">Unknown</span></td>
-                            @endif
-                        </tr>
-                                @php
-                                $rowNumber++;
-                                @endphp
-                                @endforeach
-
-                    </tbody>
-                </table>
-            @else
-            <p>No Quiz History found.</p>
-            @endif         
+                        @if ($hoursAgo <= 72) {{-- Show results for the last 3 days --}}
+                            <tr class="topic">
+                                <td class="font-weight-medium">{{ $rowNumber }}</td>
+                                <td>{{ $result->quiz->title }}</td>
+                                <td>{{ $result->score }}/{{ $result->quiz->questions->count() }}</td>
+                                <td>
+                                    <span class="timestamp">
+                                        @if ($hoursAgo === 0)
+                                            just now
+                                        @else
+                                            {{ $hoursAgo }} hours ago
+                                        @endif
+                                    </span>
+                                </td>
+                            </tr>
+                            @php
+                            $rowNumber++;
+                            @endphp
+                        @endif
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>No Quiz History found for last 3 days.</p>
+        @endif
+      
             </div>
         </div>   
     </div>       
